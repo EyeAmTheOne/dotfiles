808d1e3517febe4761cd76f714328eed2175ffdc
Gavin <68349014+EyeAmTheOne@users.noreply.github.com>
2026-07-29 08:49:50 -0700

    Add setup script and configs

---
+++ run_once_before_install-packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# chezmoi:template:left-delimiter="#" right-delimiter="#"
+
+echo "Starting system package isntallation..."
+
+# Update
+sudo pacman -Sy --noconfirm
+
+# Base applications
+sudo pacman -S --needed --noconfirm \
+  neovim \
+  discord \
+  chezmoi \
+  git \
+  fastfetch \
+  github-cli
+
+if command -v yay &>/dev/null; then
+  echo "Installing AUR packages using yay..."
+  # Add AUR packages
+  # yay -S --needed --noconfirm example-aur-package
+else
+  echo "yay not found. Skipping AUR step"
+fi
